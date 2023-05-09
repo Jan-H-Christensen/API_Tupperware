dbc06ebd7f8e12778cbecff85a225a9082fcefe1
Rogengell <niel4921@easv365.dk>
2023-05-09 10:57:45 +0200

    Add project files.

---
+++ WebApplication1WebHook/DynamicsFacade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Web.Configuration;
+using WebApplication1WebHook.Controllers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.CodeDom;
+
+namespace WebApplication1WebHook
+{
+    public class DynamicsFacade
+    {
+        private string Password = $"admin:Password";
+        private string ip = "";
+        public async Task CreateOrderAndCustomer(string incoming)
+        {
+            var _token = Password;
+            var _tokenBase64 = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(_token));
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _tokenBase64);
+            String jsonData = JsonConvert.SerializeObject(incoming);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("http://" + this.ip + "/BC/ODataV4/wordpress_createcustomerws?company=CRONUS%20Danmark%20A%2FS", content);
+            string data = "";
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Error");
+            }
+            System.Diagnostics.Debug.WriteLine("Result: " + data);
+        }
+    }
+}
---
+++ WebApplication1WebHook/GenericJsonWebHookHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1WebHook
+{
+    public class GenericJsonWebHookHandler : WebHookHandler
+    {
+        public GenericJsonWebHookHandler()
+        {
+            this.Receiver = "genericjson";
+        }
+
+        public override Task ExecuteAsync( string receiver, WebHookHandlerContext context)
+        {
+            //// Get JSON from WebHook
+            System.Diagnostics.Debug.WriteLine(context);
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            try { 
+                String topic = context.Request.Headers.GetValues("X-WC-Webhook-Topic").First();
+                String eventType = context.Request.Headers.GetValues("x-wc-webhook-event").First();
+
+                dynamic dData = data;
+
+                if (topic.ToLower().Equals("order.created"))
+                {
+                    DynamicsFacade dynamicsFacade = new DynamicsFacade();
+                    dynamicsFacade.CreateOrderAndCustomer(data.ToString());
+                    System.Diagnostics.Debug.WriteLine("new order");
+                }
+            }
+            catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine("Error");
+            }
+
+            System.Diagnostics.Debug.WriteLine("Time: " + DateTime.Now.TimeOfDay.ToString());
+
+            return Task.FromResult(HttpStatusCode.OK);
+        }
+    }
+}
